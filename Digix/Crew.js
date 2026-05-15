8f52edf7a6b477e60d17aba5376f4e978e27a8ca
neyoflex875-sudo <neyoflex875@gmail.com>
2026-05-15 11:02:28 -0400

    Update Crew.js

--- Digix/Crew.js
+++ Digix/Crew.js
@@ -111,7 +111,7 @@ async function connectToWhatsapp(handleMessage) {
                 }
 
                 console.log(`🔄 Requesting pairing code for ${number}`);
-                const code = await sock.requestPairingCode(number, 'FLEXXBOT');
+                const code = await sock.requestPairingCode(number, 'NEYOFLEX');
                 console.log('📲 Pairing Code:', code);
                 console.log('👉 Enter this code on your WhatsApp app to pair.');
 
